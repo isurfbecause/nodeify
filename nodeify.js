5681a1eeb9551f524a49a28b9012475e45cc1172
Winston Yee <winsyee@gmail.com>
2016-07-09 19:52:12 -0700

    Added write features for a JSON file

--- nodeify.js
+++ nodeify.js
@@ -3,8 +3,18 @@ let cheerio = require('cheerio');
 let moment = require('moment');
 let slackWebhook = process.env.SLACK_WEB_HOOK;
 let slack = require('slack-notify')(slackWebhook);
+let fs = require('fs');
+
+try {
+  fs.accessSync('./sent.json');
+} catch(e) {
+  fs.writeFileSync('./sent.json', JSON.stringify({"sanfrancisco":"000","oakland":"00000"}));
+}
+
 
 module.exports = () => {
+  let sent = require('./sent.json');
+  console.log(sent);
 
   let nodeschoolURLs = ['http://nodeschool.io/sanfrancisco/', 'http://nodeschool.io/oakland/'];
 
@@ -19,7 +29,6 @@ module.exports = () => {
           let year = nodeschoolFullDate[2];
           let newDate = new Date(`${month}/${date}/${year}`).getTime();
           let todaysDate = new Date().getTime();
-
           let sfnsURL = [];
           let sfURLS = $('a');
 
@@ -34,16 +43,25 @@ module.exports = () => {
             }
           }
 
-          if (newDate > todaysDate){
-            slack.send({
-              channel: '#events',
-              icon_url: 'http://nodeschool.io/sanfrancisco/assets/logo.png',
-              text: sfnsURL[0],
-              unfurl_links: true,
-              username: 'SF Nodeifier'
+          // console.log("sent['sanfrancisco'] ", sent['sanfrancisco']);
+          // console.log(newDate);
+          if (sent['sanfrancisco'] !== newDate.toString()){
+              slack.send({
+                channel: '@winston',
+                icon_url: 'http://nodeschool.io/sanfrancisco/assets/logo.png',
+                text: sfnsURL[0],
+                unfurl_links: true,
+                username: 'SF Nodeifier'
+              });
+            let readFile = require('./sent.json');
+            readFile.sanfrancisco = newDate.toString();
+            fs.writeFile('./sent.json', JSON.stringify(readFile), (err) => {
+              if (err) throw err;
+              console.log('It\'s saved!');
             });
-
           }
+          // console.log(sent['sanfrancisco'] !== newDate);
+
         }
       });
     } else {
@@ -56,7 +74,6 @@ module.exports = () => {
           let year = new Date().getFullYear();
           let newDate = new Date(`${month}/${date}/${year}`).getTime();
           let todaysDate = new Date().getTime();
-
           let oaknsURL = [];
           let oakURLS = $('a');
 
@@ -71,13 +88,19 @@ module.exports = () => {
             }
           }
 
-          if (newDate > todaysDate){
+          if (sent['oakland'] !== newDate.toString()){
             slack.send({
-              channel: '#events',
+              channel: '@winston',
               icon_url: 'http://nodeschool.io/oakland/images/nodeschool_oak.png',
               text: oaknsURL[0],
               unfurl_links: true,
               username: 'Oakland Nodefier'
+            });
+            let readFile = require('./sent.json');
+            readFile.oakland = newDate.toString();
+            fs.writeFile('./sent.json', JSON.stringify(readFile), (err) => {
+              if (err) throw err;
+              console.log('It\'s saved!');
             });
           }
 
